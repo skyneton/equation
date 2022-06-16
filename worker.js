4d293fa7d13a937dd370297fe888c82450dc3275
배동인 <67633420+skyneton@users.noreply.github.com>
2022-06-16 20:17:42 +0900

    upload

---
+++ worker.js
@@ -0,0 +1,97 @@
+const EquationRenderWorker = () => {
+    let canvas;
+    let ctx;
+    let zoom, currentX, currentY;
+    let equation;
+    let width, height;
+
+    function render() {
+        canvasClear();
+        drawZeroGrid();
+        drawGraph()
+        if(!equation) return;
+    }
+
+    function canvasClear() {
+        ctx.clearRect(-width >> 1, -height >> 1, width, height);
+    }
+
+    function drawZeroGrid() {
+        const originStyle = ctx.strokeStyle;
+        ctx.strokeStyle = "rgba(0, 0, 0, 0.7)";
+
+        ctx.beginPath();
+        ctx.moveTo(currentX, -height >> 1);
+        ctx.lineTo(currentX, height >> 1);
+        
+        ctx.moveTo(-width >> 1, currentY);
+        ctx.lineTo(width >> 1, currentY);
+
+        ctx.setLineDash([3, 3]);
+        ctx.stroke();
+
+        ctx.setLineDash([]);
+        ctx.strokeStyle = originStyle;
+    }
+
+    function drawGraph() {
+        const originStyle = ctx.strokeStyle;
+        ctx.strokeStyle = "red";
+
+        const dw = width >> 1;
+        // const dh = height >> 1;
+
+        ctx.beginPath();
+        let isMoved = false;
+        for(let x = -dw; x <= dw; x++) {
+            const posX = x * zoom - currentX;
+            const posY = -(equation.calc(posX) * zoom - currentY);
+            if(isNaN(posY)) {
+                isMoved = false;
+                continue;
+            }
+
+            if(!isMoved) {
+                isMoved = true;
+                ctx.moveTo(x, posY);
+            }
+            ctx.lineTo(x, posY);
+        }
+        ctx.stroke();
+
+        ctx.strokeStyle = originStyle;
+    }
+
+    self.addEventListener("message", e => {
+        const data = e.data;
+        switch(data.type) {
+            case "import":
+                importScripts(data.data);
+                break;
+            case "canvas":
+                canvas = data.data;
+                ctx = canvas.getContext("2d");
+                width = canvas.width;
+                height = canvas.height;
+                ctx.translate(width / 2, height / 2);
+                render();
+                break;
+            case "equation":
+                equation = new Equation(data.data);
+                render();
+                break;
+            case "resize":
+                width = canvas.width = data.data[0];
+                height = canvas.height = data.data[1];
+                ctx.translate(width >> 1, height >> 1);
+                render();
+                break;
+            case "update":
+                zoom = data.data[0];
+                currentX = data.data[1];
+                currentY = data.data[2];
+                render();
+                break;
+        }
+    });
+}
